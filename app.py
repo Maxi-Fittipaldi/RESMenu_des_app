ac73d7585aeb1c4b8b6e373a13c8293bb5dc59f7
CURSO5A <a.maximiliano.fittipaldi@schonthal.esc.edu.ar>
2022-05-17 13:20:27 -0300

    sesiones de usuario

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect, send_from_directory
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.route("/")
 def root():
         return "Welcome page"
@@ -12,11 +12,19 @@ def login():
     if request.method == "POST":
         mail = request.form["mail"]
         password = request.form["pwd"]
+        session["mail"] = mail
         return redirect("/profile")
     else:
         return render_template("login.html")
 @app.route("/profile")
 def profile():
-    return "profile"
+    if "mail" in session:
+        return f'iniciaste sesión como {session["mail"]}'
+    return "no iniciaste sesión"
+
+@app.route('/logout')
+def logout():
+    session.pop('mail', None)
+    return redirect("/login")
 if __name__ == "__main__":
    app.run(debug=True)
