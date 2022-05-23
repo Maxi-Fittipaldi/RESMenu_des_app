7f6de994f214797ea8bf2422a6efddaddd82068d
CURSO5A <a.maximiliano.fittipaldi@schonthal.esc.edu.ar>
2022-05-23 09:38:17 -0300

    eliminación de comentario

--- encrypt.py
+++ encrypt.py
@@ -4,8 +4,3 @@ def encrypt(password):
     encoded = password.encode() 
     passwordEncrypted = hashlib.sha256(encoded)
     return passwordEncrypted.hexdigest()
-
-#def verification(password):
-#    passwordEnc = password.encode()
-#    passwordEncrypted = hashlib.sha256(passwordEnc)
-#    passwordEncrypted.hexdigest() == userPasswordEncrypted.hexdigest()
