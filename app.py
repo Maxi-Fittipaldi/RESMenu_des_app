e204bf561ba7c8ba8a933e6523d969a8f2eeb50a
CURSO5A <nicogalo17@gmail.com>
2022-05-30 08:35:55 -0300

    cambios en /manage

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def profile():
         return render_template("profile.html")
     return "no iniciaste sesión"
 
-@app.route("products/select/<int:id>", methods=['GET','POST'])
+@app.route("manage/search", methods=['GET','POST'])
 def search():
     if request.method == "POST":
         productName = request.form["name"]
@@ -80,13 +80,13 @@ def search():
     else:
         return render_template("productos.html")
 
-@app.route("products/select/<int:id>")
+@app.route("manage/select")
 def select():
     db.session.execute("SELECT * FROM products")
     db.session.commit()
     return redirect("/")
 
-@app.route("/products/insert/<int:id>", methods=['GET','POST'])
+@app.route("manage/insert/<int:id>", methods=['GET','POST'])
 def insert(): 
     if request.method == "POST":
         productName = request.form["name"]
@@ -100,13 +100,18 @@ def insert():
         products = db.session.execute("SELECT * FROM products")
         return render_template('index.html',products=products)
 
-@app.route("/delete/<int:id>")
+@app.route("manage/delete/<int:id>")
 def delete(id):
-    db.session.execute("DELETE FROM products WHERE id = :id",{"id":id})
-    db.session.commit()
-    return redirect("/")
-    
-@app.route("/update/<int:id>", methods=["GET","POST"])
+    if request.method == "POST":
+        productid = request.form["id"]
+        db.session.execute("DELETE FROM products WHERE id = :id",{"id":productid})
+        db.session.commit()
+        return redirect("/")
+    else:
+        products = db.session.execute("SELECT * FROM products")
+        return render_template('index.html',products=products)
+
+@app.route("manage/update/<int:id>", methods=["GET","POST"])
 def update(id):
     if request.method == "POST":
         productName = request.form["name"]
@@ -125,3 +130,4 @@ def logout():
     return redirect("/login")
 if __name__ == "__main__":
    app.run(debug=True)
+
