d08915eed1f4b90a52113918beb093169e661434
CURSO5A <nicogalo17@gmail.com>
2022-06-27 09:27:07 -0300

    verify log

--- profile.py
+++ profile.py
@@ -3,6 +3,6 @@ from head import *
 def profile():
     @app.route("/profile")
     def profile():
-        if "id" in session:
-            return render_template("profile.html")
-        return "no iniciaste sesión"
+        if not "id" in session:
+            return redirect("/login")
+        return render_template("profile.html")
