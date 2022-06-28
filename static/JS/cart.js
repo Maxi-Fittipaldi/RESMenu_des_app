3547e17b06941ae90c875e16622429134afa76d7
CURSO5A <a.maximiliano.fittipaldi@schonthal.esc.edu.ar>
2022-06-28 12:54:02 -0300

    correcciones en el funcionamiento del carro

--- static/JS/cart.js
+++ static/JS/cart.js
@@ -1,9 +1,13 @@
 const productsInCart = document.getElementsByClassName("products-in-cart")[0];
 const productsContainer = document.getElementsByClassName("products-container")[0];
+
 const onClick = (event) => {
     const productId = event.srcElement.id;
     const product = productsContainer.querySelector("[name='"+productId+"']");
     const clonedProd = product.cloneNode(true);
+    addToCartButton = clonedProd.getElementsByTagName("button")[0];
+    addToCartButton.remove();
+    console.log(clonedProd)
     productsInCart.appendChild(clonedProd);
 }
 window.addEventListener("click",onClick);
