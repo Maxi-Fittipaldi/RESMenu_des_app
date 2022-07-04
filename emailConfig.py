f8adbc4d8cff445435b40bdb11f19bd028c3c177
CURSO5A <nicogalo17@gmail.com>
2022-07-04 09:41:17 -0300

    verificacion-mail

---
+++ emailConfig.py
@@ -0,0 +1,24 @@
+class BaseConfig(object):
+    """Base configuration."""
+
+    # main config
+    SECRET_KEY = 'my_precious'
+    SECURITY_PASSWORD_SALT = 'my_precious_two'
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    WTF_CSRF_ENABLED = True
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    # mail settings
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+    # gmail authentication
+    MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
+
+    # mail accounts
+    MAIL_DEFAULT_SENDER = 'from@example.com'
