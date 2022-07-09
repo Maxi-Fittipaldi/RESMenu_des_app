f00de8ca01ae90f1dcc5d56c1b6759ef22cff124
Maxi Fittip <maxi.fittipaldi@gmail.com>
2022-07-09 19:57:44 -0300

    esquema blueprint funcional

--- indexpage.py
+++ indexpage.py
@@ -1,5 +1,5 @@
-from head import *
+from flask import Blueprint, render_template, request, url_for, redirect, send_from_directory, session, flash
+bp = Blueprint('indexpage', __name__, url_prefix='/')
+@bp.route("/")
 def root():
-    @app.route("/")
-    def root():
-            return "Welcome page"
+        return "Welcome page"
--- profile.py
+++ profile.py
@@ -1,8 +1,7 @@
-from head import *
-
+from flask import Blueprint, render_template, request, url_for, redirect, send_from_directory, session, flash
+bp = Blueprint('profile', __name__, url_prefix='/')
+@bp.route("/profile")
 def profile():
-    @app.route("/profile")
-    def profile():
-        if "id" in session:
-            return render_template("profile.html")
-        return "no iniciaste sesión"
+    if "id" in session:
+        return render_template("profile.html")
+    return "no iniciaste sesión"
