8c73b2d9d3d920488ac5cf00050801b0b9eea3b0
Maxi Fittip <a.maximiliano.fittipaldi@schonthal.esc.edu.ar>
2022-07-10 13:59:31 -0300

    modificaciones al menu

--- menu.py
+++ menu.py
@@ -1,13 +1,14 @@
-from head import *
+from flask import Blueprint, render_template, request, url_for, redirect, send_from_directory, session, flash
+from RESMenu_des_app import db
+bp = Blueprint('menu', __name__, url_prefix='/')
+
+@bp.route("/menu", methods=['GET'])
 def menu():
-    @app.route("/menu", methods=['GET'])
-    def menu():
-        productos = db.session.execute("SELECT * FROM productos")
-        return render_template("menu.html",productos=productos)
+    productos = db.session.execute("SELECT * FROM productos")
+    return render_template("menu.html",productos=productos)
+@bp.route("/menu/commit",methods=["POST"])
 def commit():
-    @app.route("/menu/commit",methods=["POST"])
-    def commit():
-        content_type = request.headers.get("Content-Type")
-        json = request.json()
-        print(json["product_ids"])
-        return redirect("/menu")
+    content_type = request.headers.get("Content-Type")
+    json = request.json()
+    print(json["product_ids"])
+    return redirect("/menu")
