732909e4203d21cfd090ae048d41e4e8b33f4111
Instalador Colegio <nicogalo17@gmail.com>
2022-08-22 09:40:00 -0300

    modificar ordenes

--- orders.py
+++ orders.py
@@ -11,4 +11,24 @@ def orders():
     if session["state"] == "pendiente":
         return redirect("/profile")
     orders = db.session.execute("""SELECT * FROM cabeceraTransaccion""")
-    return render_template("orders.html", orders=orders)
+    return render_template("orders.html", cabeceras_transaccion=orders)
+
+@bp.route("/orders/update/<int:id>", methods=["POST"])
+def update(id):
+    if not "id" in session:
+        return redirect("/login")
+    if session["state"] == "pendiente":
+        return redirect("/profile")
+    if request.method == "POST":
+        estado = request.form ["estado"]
+        print(estado)
+        db.session.execute("""UPDATE cabeceraTransaccion
+        SET estado=:e
+        WHERE id = :id
+        """,
+        {
+        "e" :estado,
+        "id" :id
+        })
+        db.session.commit()
+        return redirect("/orders")
