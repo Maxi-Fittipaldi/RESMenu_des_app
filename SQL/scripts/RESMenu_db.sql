db3a7e80348ffe32ede447edabb2e74a7780920a
Maxi Fittipaldi <a.maximiliano.fittipaldi@schonthal.esc.edu.ar>
2022-10-17 09:40:13 -0300

    modif db

--- SQL/scripts/RESMenu_db.sql
+++ SQL/scripts/RESMenu_db.sql
@@ -16,7 +16,7 @@ CREATE TABLE usuarios (
 
 CREATE TABLE cabeceraTransaccion(
     id INT(11) NOT NULL AUTO_INCREMENT,
-    usuario_id INT(11) NOT NULL,
+    cliente_nombre VARCHAR(50) NOT NULL,
     estado ENUM("completado","en_proceso","pendiente","expirado") DEFAULT "pendiente",
     fecha datetime DEFAULT current_timestamp,
     PRIMARY KEY(id),
